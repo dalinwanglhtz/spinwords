9e0b1549c8b0df22e4997af09a959e928184426e
Dale Wang <dalinwanglhtz@gmail.com>
2020-03-17 15:02:27 +1100

    first working cut

--- src/main/java/com/codewardev/SpinWords.java
+++ src/main/java/com/codewardev/SpinWords.java
@@ -1,12 +1,27 @@
 package com.codewardev;
 
-// https://www.codewars.com/kata/5264d2b162488dc400000001/train/java
+//https://www.codewars.com/kata/5264d2b162488dc400000001/train/java
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 
 public class SpinWords {
 
 	public String spinWords(String sentence) {
+		String[] words = sentence.split(" ");
+		String result ="";
+		result = Arrays.stream(words).map(x-> {
+			if(x.length() >= 5) {
+				StringBuilder a = new StringBuilder(x);
+				x = a.reverse().toString();
+			}
+			return x;
+		}).collect(Collectors.joining(" "));
 		
-		return null;
+		System.out.println("Result: "+result);
+		
+		return result;
 	}
 
 }
