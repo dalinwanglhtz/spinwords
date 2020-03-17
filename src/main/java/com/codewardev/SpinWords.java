958555d9eb648c4bb6bb70211688ac5904461095
Dale Wang <dalinwanglhtz@gmail.com>
2020-03-17 15:18:51 +1100

    refined working cut

--- src/main/java/com/codewardev/SpinWords.java
+++ src/main/java/com/codewardev/SpinWords.java
@@ -9,19 +9,9 @@ import java.util.stream.Collectors;
 public class SpinWords {
 
 	public String spinWords(String sentence) {
-		String[] words = sentence.split(" ");
-		String result ="";
-		result = Arrays.stream(words).map(x-> {
-			if(x.length() >= 5) {
-				StringBuilder a = new StringBuilder(x);
-				x = a.reverse().toString();
-			}
-			return x;
+		return Arrays.stream(sentence.split(" ")).map(x-> {
+			return x.length() >= 5 ? new StringBuilder(x).reverse().toString():x;
 		}).collect(Collectors.joining(" "));
-		
-		System.out.println("Result: "+result);
-		
-		return result;
 	}
 
 }
